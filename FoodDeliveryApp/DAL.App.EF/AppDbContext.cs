404348191cbe0ee4f3e88a2f98f91dad97537882
arpahh <arpahh@ttu.ee>
2020-05-04 16:01:06 +0300

    base done

---
+++ FoodDeliveryApp/DAL.App.EF/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.App.EF {
+
+    public class AppDbContext : IdentityDbContext {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+
+}
