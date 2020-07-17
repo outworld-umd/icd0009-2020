bbe93fa705cbd00caf7daac6a5cfa8228cebae62
Artjom Pahhomov <arpahh@ttu.ee>
2020-07-17 22:09:58 +0300

    login via jwt done

--- food-delivery-vue/src/store/index.ts
+++ food-delivery-vue/src/store/index.ts
@@ -3,7 +3,9 @@ import Vuex from 'vuex'
 import { Locales } from "@/i18n/locales";
 import createPersistedState from 'vuex-persistedstate';
 import { defaultLocale } from "@/i18n";
-import { Module, VuexModule, Mutation } from 'vuex-module-decorators'
+import {Module, VuexModule, Mutation, Action} from 'vuex-module-decorators'
+import {ILoginRequest} from "@/domain/identity/ILoginRequest";
+import { AccountAPI } from "@/services/AccountAPI";
 
 Vue.use(Vuex)
 
@@ -11,7 +13,11 @@ Vue.use(Vuex)
 export class UserModule extends VuexModule {
     jwt: string | null = null;
     role: string | null = null;
-    lang: Locales = defaultLocale
+    lang: Locales = defaultLocale;
+
+    get isAuthenticated(): boolean {
+        return this.jwt !== null;
+    }
 
     @Mutation
     setLanguage(lang: Locales) {
@@ -27,6 +33,20 @@ export class UserModule extends VuexModule {
     setRole(role: string) {
         this.role = role;
     }
+
+    @Mutation
+    clearJwt(): void {
+        this.jwt = null;
+    };
+
+    @Action
+    async authenticateUser(loginDTO: ILoginRequest): Promise<boolean> {
+        const jwt = await AccountAPI.login(loginDTO);
+        this.context.commit('setJwt', jwt);
+        return jwt !== null;
+    }
+
+
 }
 
 export default new Vuex.Store({
